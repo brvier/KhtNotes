a0dde31f3ac65414e7c52adcfd9931d09e64171a
Benoit HERVIER <khertan@khertan.net>
2012-07-19 22:57:44 +0200

    Implement parts of webdav sync logic, changes settings class, change qml prefs ui not yet finished

--- khtnotes/sync.py
+++ khtnotes/sync.py
@@ -13,6 +13,9 @@
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 
+import sys
+from webdav.WebdavClient import *
+import os.path
 import os
 import urllib2
 import urllib
@@ -24,6 +27,10 @@ from note import Note
 from settings import Settings
 import decimal
 import time
+import json
+
+def basename(path):
+  return os.path.basename(path)
 
 class Sync(QObject):
   '''Sync class'''
@@ -40,131 +47,152 @@ class Sync(QObject):
         self.thread = threading.Thread(target=self._sync)
         self.thread.start()
 
-  def _get_data(self, timedelta=0):
-    
-    data = {}
-    index = {}    
-    
-    notes = [Note(uid=file) for file in os.listdir(Note.NOTESPATH) if os.path.isfile(os.path.join(Note.NOTESPATH, file))]        
-    delnotes = [file for file in os.listdir(Note.DELETEDNOTESPATH) if os.path.isfile(os.path.join(Note.DELETEDNOTESPATH, file))]
-
-    for note in notes:
-        print 'DEBUG: UUID:', note.uuid, 'Timespamp:', self.toJsTimestamp(note.timestamp - timedelta)
-        index[note.uuid] = {'timestamp': self.toJsTimestamp(note.timestamp - timedelta),
-                            'title':note.title,}
-        data[note.uuid] = json.dumps({"entry-id":note.uuid,"entry-content":note.data})
-
-    for noteuuid in delnotes:
-        index[noteuuid] = {'timestamp':0,
-                            'title':'',}                            
-        data[noteuuid] = json.dumps({"entry-id":noteuuid,"entry-content":''})
-        
-    data['index'] = json.dumps(index)
-    data['clientime'] = self.toJsTimestamp(time.time())
-    
-    return (data, index, notes, delnotes)
-
-  def toPyTimeStamp(self, jstimestamp):
-    ''' Convert Javascript timestamp (since epoch in milliseconds) to Unix timestamp'''
-    if type(jstimestamp) == unicode:
-        return decimal.Decimal(jstimestamp) / 1000
-    return decimal.Decimal(unicode(jstimestamp / 1000))
-    
-  def toJsTimestamp(self, pytimestamp):
-    ''' Convert Unix timestamp (since epoch in seconds) to Javascript timestamp'''
-    if type(pytimestamp) == unicode:
-        return long(pytimestamp) * 1000
-    return long(pytimestamp * 1000)
-        
   def _sync(self):
-    ''' Sync the notes'''
-    
+    '''Sync the notes with a webdav server'''
+    #Read Settings
+    settings = Settings()
+    webdavHost = settings.webdavHost
+    self.webdavBasePath = settings.webdavBasePath
+    webdavLogin = settings.webdavLogin
+    webdavPasswd = settings.webdavPasswd
+
+    #Create Connection
+    isConnected = False
+    webdavConnection = CollectionStorer(webdavHost+self.webdavBasePath,
+                                        validateResourceNames=False)
+    #Test KhtNotes folder and authenticate
+    authFailures = 0
+    while authFailures < 3:
+      try:
+        webdavConnection.validate()
+        isConnected = True
+        break # break out of the authorization failure counter
+      except AuthorizationError, e:
+        if e.authType == "Basic":
+          webdavConnection.connection.addBasicAuthorization(webdavLogin, webdavPasswd)
+        elif e.authType == "Digest":
+          info = parseDigestAuthInfo(e.authInfo)
+          webdavConnection.connection.addDigestAuthorization(webdavLogin, webdavPasswd, realm=info["realm"], qop=info["qop"], nonce=info["nonce"])
+        elif authFailure >= 2:
+          print 'Wrong login password'
+        else:
+          print type(e), e
+          self.on_error.emit(unicode(e) +':'+unicode(e))
+
+      except Exception, err:
+        self.on_error.emit(unicode(type(err))+':'+unicode(err))
+        print unicode(type(err))+':'+unicode(err)
+      authFailures += 1
+
+    if isConnected:
+      self._check_khtnotes_folder_and_lock(webdavConnection)
+      index_remote, deleted_remote = self._get_remote_index(webdavConnection)
+      index_local, deleted_local = self._get_local_index()
+      self._write_remote_index(webdavConnection)
+      print 'Remote:', index_remote
+      print 'Local:', index_local
+      #Remotly deleted
+      for filename in set(index_local).intersection(set(deleted_remote)):
+        if index_local[filename]>deleted_remote[filename]:
+          self._upload(webdavConnection, filename)
+        else:
+          self._local_delete(filename)
+      #Locally deleted
+      for filename in set(index_remote).intersection(set(deleted_local)):
+        if index_remote[filename]>deleted_remote[filename]:
+          self._download(webdavConnection, filename)
+        else:
+          self._remote_delete(webdavConnection, filename)
+      #Updated
+      for filename in set(index_remote).intersection(set(index_local)):
+        if index_remote[filename]>index_remote[filename]:
+          self._download(webdavConnection, filename)
+        elif index_remote[filename]<index_remote[filename]:
+          self._upload(webdavConnection, filename)
+        #Else we are already uptodate
+      #New remote
+      for filename in set(index_remote) - (set(index_local)):
+        self._download(webdavConnection, filename)
+      #New local
+      for filename in set(index_local) - (set(index_remote)):
+        self._upload(webdavConnection, filename)
+
+      self._unlock(webdavConnection)
+
+  def _upload(self, webdavConnection, filename):
+    #TODO
+    print 'DEBUG: Upload', filename
+
+  def _download(self, webdavConnection, filename):
+    #TODO
+    print 'DEBUG: Download', filename
+
+  def _remote_delete(self, webdavConnection, filename):
+    #TODO
+    print 'DEBUG: remote_delete', filename
+
+  def _local_delete(self, filename):
+    #TODO
+    print 'DEBUG: locale_delete', filename
+
+  def _write_remote_index(self, webdavConnection):
+    #TODO
+    pass
+
+  def _unlock(self,filename):
+    #TODO
+    pass
+
+  def _get_notes_path(self):
+    khtnotesPath = self.webdavBasePath
+    if not khtnotesPath.endswith('/'):
+       return khtnotesPath + '/KhtNotes/'
+    else:
+       return khtnotesPath + 'KhtNotes/'
+
+  def _check_khtnotes_folder_and_lock(self, webdavConnection):
+    '''Check that khtnotes folder exists on webdav'''
+    khtnotesPath = self._get_notes_path()
+    if not khtnotesPath in webdavConnection.listResources().keys():
+      webdavConnection.addCollection(khtnotesPath)
+    #TODO : Lock
+
+  def _get_remote_index(self, webdavConnection):
+    '''Check Remote Index'''
+    webdavConnection.path = self._get_notes_path() + '.deleted.sync'
     try:
-        settings = Settings()
+      index = json.loads(webdavConnection.downloadContent())
+    except WebdavError, err:
+      if err.reason == 'Not Found':
+         index = {'current':{}, 'deleted':{}}
+      else:
+         raise
+    webdavConnection.path = self._get_notes_path()
+    new_index = dict([(basename(resource), time.mktime(properties.getLastModified())) \
+            for (resource, properties) \
+            in webdavConnection.listResources().items()])
+    for filename, timestamp in index['current']:
+      if filename not in new_index.key():
+        index['deleted'][filename] = timestamp
+    return (new_index, index['deleted'])
 
-        #Dummy request to calc time delta with server
-        urlfordummyrequest = urlparse.urlparse(settings.syncUrl)
-        response = urllib2.urlopen(urlfordummyrequest.scheme +'://'+urlfordummyrequest.netloc, urllib.urlencode({}))
-        remote_time_str = response.info()['Date']
-        try:
-            time_delta = time.mktime(time.strptime(remote_time_str, '%a, %d %b %Y %H:%M:%S %Z')) - time.mktime(time.gmtime())
-        except:
-            try:
-                time_delta = time.mktime(time.strptime(remote_time_str, '%a, %d %b %Y %H:%M:%S +0000')) - time.mktime(time.gmtime())
-            except:
-                print 'Can t get server time'
-                time_delta = 0                
-        print 'Time delta with server:', time_delta
-        
-        #Time delta require that all client use it, which isn't the case
-        #yet for the web client        
-        time_delta = 0        
+  def _get_local_index(self):
+    index = dict([(filename, os.path.getmtime(os.path.join(Note.NOTESPATH, filename))) for filename in os.listdir(Note.NOTESPATH)
+                    if os.path.isfile(os.path.join(Note.NOTESPATH, filename))])
+    delnotes = dict([(filename, os.path.getmtime(os.path.join(Note.DELETEDNOTESPATH, filename))) for filename in os.listdir(Note.NOTESPATH)
+                    if os.path.isfile(os.path.join(Note.DELETEDNOTESPATH, filename))])
+    return (index, delnotes)
 
-        passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        passman.add_password(None, settings.syncUrl, settings.syncLogin, settings.syncPassword)
-        authhandler = urllib2.HTTPBasicAuthHandler(passman)        
-        opener = urllib2.build_opener(authhandler)    
-        urllib2.install_opener(opener)
-        local_data,local_index, local_notes , local_delnotes = self._get_data()
-        print settings.syncUrl
-        response = urllib2.urlopen(settings.syncUrl, urllib.urlencode(local_data))
-        
-        response = response.read()
-        print response
-        remote_data = json.loads(response)
-        remote_index = remote_data['index']
-        remote_entries = remote_data['entries']
+  def _get_local_data(self, timedelta=0):
+    data = {}
+    index = {}
 
-        #As server sync reply only what should exit we delete what don't exist on server
-        [Note(uid).rm() for uid in [note.uuid for note in local_notes] if uid not in remote_index]        
+    notes = [Note(uid=file) for file in os.listdir(Note.NOTESPATH)
+                    if os.path.isfile(os.path.join(Note.NOTESPATH, file))]
+    delnotes = [file for file in os.listdir(Note.DELETEDNOTESPATH)
+                    if os.path.isfile(os.path.join(Note.DELETEDNOTESPATH, file))]
 
-        if not remote_entries == []: #Server return 0 entries if nothing changes
-            for rindex in remote_index:
-                #remote_index = json.loads(rindex)
-                ridata = remote_index[rindex]
-                remote_timestamp = self.toPyTimeStamp(ridata['timestamp']) + decimal.Decimal(unicode(time_delta))
-                #if remote_timestamp > 0 :
-                #    remote_timestamp = remote_timestamp - decimal.Decimal(unicode(time_delta))
-                if rindex in local_index:
-                    #Server sync.php compute everythings, integrate directly
-                    note = Note(uid=rindex)
-                    print 'DEBUG: Remote timestamp:', time.localtime(float(remote_timestamp)), 'Local timestamp:', time.localtime(float(self.toPyTimeStamp(local_index[rindex]['timestamp']))), 'Title:', note.title
-                    if rindex in remote_entries: #server think there is nothing new
-                        note.write(remote_entries[rindex]['entry-content']) 
-                    print remote_timestamp, ':', ridata['timestamp']
-                    note.overwrite_timestamp(float(remote_timestamp ))
 
-                    #local_timestamp = self.toPyTimeStamp(local_index[rindex]['timestamp'])
-                    #print 'DEBUG: local, remote', local_timestamp , remote_timestamp, Note(rindex).title
-                    #if remote_timestamp == 0 : #Remote entry has been deleted, remove the local too
-                    #    Note(rindex).rm()
-                    #elif os.path.exists(os.path.join(Note.DELETEDNOTESPATH,rindex)): #Local entry has been deleted, remove lo   
-                    #    pass
-                    #elif remote_timestamp > local_timestamp: # Remote entry is newer, get it                    
-                    #    #print 'DEBUG remote entry newer : ' , remote_entries[rindex]
-                    #    if rindex in remote_entries: #server think there is nothing new
-                    #        note = Note(uid=rindex)
-                    #        note.write(remote_entries[rindex]['entry-content'])
-                    #        note.overwrite_timestamp(float(remote_timestamp))
-                    #elif remote_timestamp < local_timestamp: # Local entry is newer, don't get it
-                    #    pass
-                    #else:
-                    #    pass
-                else:    # Else we store it
-                    note = Note(uid=rindex)
-                    note.write(remote_entries[rindex]['entry-content']) 
-                    print remote_timestamp, ':', ridata['timestamp']
-                    note.overwrite_timestamp(float(remote_timestamp ))
- 
-
-    except Exception, e:
-        import traceback
-        traceback.print_exc()
-        print type(e), ':', e
-        self.on_error.emit(str(e))
-    
-    self._set_running(False)
-                  
   def _write(self, uid, data, timestamp=None):
     ''' Write the document to a file '''
     note = Note(uid=uid)
@@ -177,11 +205,11 @@ class Sync(QObject):
   def _set_running(self, b):
     self._running = b
     self.on_running.emit()
- 
+
   on_error = Signal(unicode)
   on_running = Signal()
-  running = Property(bool, _get_running, _set_running, notify=on_running) 
+  running = Property(bool, _get_running, _set_running, notify=on_running)
 
 if __name__ == '__main__':
   s = Sync()
-  s.launch()      
+  s.launch()
